399645fd7ffcb414f5031a65397baa3ac2a48c76
dam <dam@172.26.83.237>
2020-01-08 12:54:16 +0100

    Tercer cambio

--- Compartir/src/compartir/Compartir.java
+++ Compartir/src/compartir/Compartir.java
@@ -17,6 +17,8 @@ public class Compartir {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("8 de enero de 2020");
+        System.out.println("Rojo");
+        System.out.println("Verde");
     }
     
 }
